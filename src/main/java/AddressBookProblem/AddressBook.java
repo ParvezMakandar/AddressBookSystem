1254d71d7350cadaf6664a45d7d3ad626aa4b22f
parvez050 <parvezhm050@gmail.com>
2022-03-10 20:20:25 +0530

    Welcome To Address Book System

---
+++ src/main/java/AddressBookProblem/AddressBook.java
@@ -0,0 +1,7 @@
+package AddressBookProblem;
+
+public class AddressBook {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Address Book System");
+    }
+}
---
+++ src/test/java/AddressBookProblem/AddressBookMainTest.java
@@ -0,0 +1,16 @@
+package AddressBookProblem;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressBookMainTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+
+}
